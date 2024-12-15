fa80f372b786ef9a49227480e27d627ea70ae8f6
Andrey <rantsev001@gmail.com>
2024-12-15 15:29:02 +0300

    feat: added burger scripts

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,12 @@ const header = document.querySelector("#header");
 
 const handlerEvent = (event) => {
     const isBtn = event.target.closest("[data-sublist-button]");
+    const isBurger = event.target.closest("#burger");
+
+    if (isBurger) {
+        isBurger.classList.toggle("active");
+    }
+
     if (isBtn) {
         const menuItem = isBtn.closest("[data-menu-item]");
         const isActive = menuItem.classList.contains("active");
